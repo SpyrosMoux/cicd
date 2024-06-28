d9c2218debb909910b7d705d59b554bd5a04453e
Spyros Mouchlianitis <spirosgsaaa@gmail.com>
2024-06-29 00:43:50 +0300

    test

--- internal/handlers/webhook.go
+++ internal/handlers/webhook.go
@@ -1,28 +1,40 @@
 package handlers
 
 import (
+	"encoding/json"
 	"github.com/gin-gonic/gin"
 	"io"
+	"log"
 	"net/http"
-	"os"
-	"spyrosmoux/api/internal/queue"
+	"spyrosmoux/api/internal/models"
 )
 
 func HandleWebhook(c *gin.Context) {
 	body, err := io.ReadAll(c.Request.Body)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "Error reading body"})
+		log.Panicf("Error reading body: %v", err)
 		return
 	}
 
 	if len(body) == 0 {
 		c.JSON(http.StatusBadRequest, gin.H{"error": "Empty body"})
+		log.Panicf("Error reading body: %v", err)
 		return
 	}
 
+	var result models.GhPushWebhook
+	err = json.Unmarshal(body, &result)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		log.Panicf("Error unmarshalling body: %v", err)
+	}
+
+	log.Println(result)
+
 	// Publish the raw YAML body as a job
-	file, _ := os.ReadFile("sample-pipeline.yaml") // TODO use body instead
-	queue.PublishJob(string(file))
+	// file, _ := os.ReadFile("sample-pipeline.yaml") // TODO use body instead
+	// queue.PublishJob(string(file))
 
 	c.Status(http.StatusAccepted)
 }
---
+++ internal/models/models.go
@@ -0,0 +1,75 @@
+package models
+
+type JobModel struct {
+	BuildId     string `json:"build_id"`
+	BuildNumber int    `json:"build_number"`
+	Repository  string `json:"repository"`
+	Branch      string `json:"branch"`
+	Author      string `json:"author"`
+}
+
+// GhPushWebhook struct is the object returned
+// by GitHub's Push Webhook
+type GhPushWebhook struct {
+	After      string     `json:"after"`
+	BaseRef    string     `json:"base_ref"`
+	Before     string     `json:"before"`
+	Commits    []Commits  `json:"commits"`
+	Compare    string     `json:"compare"`
+	Created    bool       `json:"created"`
+	Deleted    bool       `json:"deleted"`
+	Forced     bool       `json:"forced"`
+	HeadCommit HeadCommit `json:"head_commit"`
+	Pusher     Pusher     `json:"pusher"`
+	Ref        string     `json:"ref"`
+	Repository string     `json:"repository"`
+}
+
+type Commits struct {
+	Added     []string `json:"added"`
+	Author    Author   `json:"author"`
+	Commiter  Commiter `json:"committer"`
+	Distinct  bool     `json:"distinct"`
+	Id        string   `json:"id"`
+	Message   string   `json:"message"`
+	Modified  []string `json:"modified"`
+	Removed   []string `json:"removed"`
+	Timestamp string   `json:"timestamp"`
+	TreeId    string   `json:"tree_id"`
+	Url       string   `json:"url"`
+}
+
+type Author struct {
+	Date     string `json:"date"`
+	Email    string `json:"email"`
+	Name     string `json:"name"`
+	Username string `json:"username"`
+}
+
+type Commiter struct {
+	Date     string `json:"date"`
+	Email    string `json:"email"`
+	Name     string `json:"name"`
+	Username string `json:"username"`
+}
+
+type HeadCommit struct {
+	Added     []string `json:"added"`
+	Author    Author   `json:"author"`
+	Commiter  Commiter `json:"committer"`
+	Distinct  bool     `json:"distinct"`
+	Id        string   `json:"id"`
+	Message   string   `json:"message"`
+	Modified  []string `json:"modified"`
+	Removed   []string `json:"removed"`
+	Timestamp string   `json:"timestamp"`
+	TreeId    string   `json:"tree_id"`
+	Url       string   `json:"url"`
+}
+
+type Pusher struct {
+	Date     string `json:"date"`
+	Email    string `json:"email"`
+	Name     string `json:"name"`
+	Username string `json:"username"`
+}
