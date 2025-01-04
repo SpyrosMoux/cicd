f36195db1b62972feafbd5cac92383e41640ca2b
Spyros Mouchlianitis <spirosgsaaa@gmail.com>
2025-01-04 14:32:06 +0200

    update domain

--- runner/git/git.go
+++ runner/git/git.go
@@ -4,12 +4,13 @@ import (
 	"fmt"
 	"log/slog"
 	"os/exec"
+	"path/filepath"
 
 	"github.com/spyrosmoux/cicd/common/dto"
 )
 
 func CloneRepo(repoMeta dto.Metadata, dir string) error {
-	repoUrl := "github.com/" + repoMeta.RepoOwner + "/" + repoMeta.Repository + ".git"
+	repoUrl := repoMeta.RepoOwner + "/" + repoMeta.Repository + ".git"
 
 	isPrivate, err := isPrivate(repoMeta.RepoVisibility)
 	if err != nil {
@@ -18,37 +19,51 @@ func CloneRepo(repoMeta dto.Metadata, dir string) error {
 
 	var baseUrl string
 	if isPrivate {
-		baseUrl = "https://x-access-token:" + repoMeta.VcsToken + "@"
+		baseUrl = "https://x-access-token:" + repoMeta.VcsToken + "@github.com/"
 	} else {
-		baseUrl = "https://"
+		baseUrl = "https://github.com/"
 	}
 
 	normalizedUrl := baseUrl + repoUrl
 
-	cmd := exec.Command("git", "clone", normalizedUrl, dir+"/"+repoMeta.Repository)
+	targetDir := filepath.Join(dir, repoMeta.Repository)
+
+	cmd := exec.Command("git", "clone", normalizedUrl, targetDir)
+
+	slog.Info("executing", "cmd", cmd.String())
+
 	output, err := cmd.CombinedOutput()
 	if err != nil {
-		return err
+		return fmt.Errorf("git clone failed output=%s", string(output))
 	}
-	slog.Info("Output: " + string(output))
+
+	slog.Info("git clone succeeded", "output", string(output))
 	return nil
 }
 
 func CheckoutBranch(branchName string) error {
 	cmd := exec.Command("git", "fetch", "origin")
+
+	slog.Info("executing", "cmd", cmd.String())
+
 	output, err := cmd.CombinedOutput()
 	if err != nil {
 		return fmt.Errorf("error fetching origin, %s", err.Error())
 	}
-	slog.Info("Output: " + string(output))
+	slog.Info("executed successfully", "cmd", cmd.String(), "output", string(output))
 
-	cmd = exec.Command("git", "checkout", "-b", branchName, "origin/"+branchName)
+	// TODO(@spyrosmoux) add check -> if branch already exists, skip checkout
+
+	cmd = exec.Command("git", "switch", "-c", branchName, "origin/"+branchName)
+
+	slog.Info("executing", "cmd", cmd.String())
+
 	output, err = cmd.CombinedOutput()
 	if err != nil {
 		return fmt.Errorf("error checking out remote branch %s, %s", branchName, err.Error())
 	}
 
-	slog.Info("Output: " + string(output))
+	slog.Info("executed successfully", "cmd", cmd.String(), "output", string(output))
 	return nil
 }
 
