d63083aee0ae04d8da2221671bd22fd8a9f2f43c
Spyros Mouchlianitis <spirosgsaaa@gmail.com>
2025-02-10 22:07:54 +0200

    fix indents

--- api/routes/routes.go
+++ api/routes/routes.go
@@ -1,50 +1,50 @@
 package routes
 
 import (
-    "github.com/gin-contrib/cors"
-    "github.com/gin-gonic/gin"
-    "github.com/spyrosmoux/cicd/api/config"
-    "github.com/spyrosmoux/cicd/api/gh"
-    "github.com/spyrosmoux/cicd/api/pipelineruns"
-    "github.com/spyrosmoux/cicd/common/dto"
-    "github.com/spyrosmoux/cicd/common/logger"
-    "net/http"
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"github.com/spyrosmoux/cicd/api/config"
+	"github.com/spyrosmoux/cicd/api/gh"
+	"github.com/spyrosmoux/cicd/api/pipelineruns"
+	"github.com/spyrosmoux/cicd/common/dto"
+	"github.com/spyrosmoux/cicd/common/logger"
+	"net/http"
 )
 
 func SetupRouter() *gin.Engine {
-    router := gin.New()
+	router := gin.New()
 
-    router.Use(gin.Recovery())
+	router.Use(gin.Recovery())
 
-    /* SuperTokens Routers */
+	/* SuperTokens Routers */
 
-    // CORS
-    router.Use(cors.New(cors.Config{
-        AllowOrigins:     []string{"http://localhost:3000", "http://localhost:63342"},
-        AllowMethods:     []string{"GET", "POST", "DELETE", "PUT", "OPTIONS"},
-        AllowCredentials: true,
-    }))
+	// CORS
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:3000", "http://localhost:63342"},
+		AllowMethods:     []string{"GET", "POST", "DELETE", "PUT", "OPTIONS"},
+		AllowCredentials: true,
+	}))
 
-    pipelineRunsRepo := pipelineruns.NewRepository(config.DB)
-    pipelineRunsSvc := pipelineruns.NewService(pipelineRunsRepo)
-    pipelineRunsHandler := pipelineruns.NewHandler(pipelineRunsSvc)
+	pipelineRunsRepo := pipelineruns.NewRepository(config.DB)
+	pipelineRunsSvc := pipelineruns.NewService(pipelineRunsRepo)
+	pipelineRunsHandler := pipelineruns.NewHandler(pipelineRunsSvc)
 
-    logs := logger.NewLogger()
+	logs := logger.NewLogger()
 
-    ghService := gh.NewService(pipelineRunsSvc, logs)
-    ghHandler := gh.NewHandler(ghService, logs)
+	ghService := gh.NewService(pipelineRunsSvc, logs)
+	ghHandler := gh.NewHandler(ghService, logs)
 
-    apiGroup := router.Group("/app/cicd/api")
-    {
-        gh.Routes(apiGroup, ghHandler)
-        pipelineruns.Routes(apiGroup, pipelineRunsHandler)
-        apiGroup.GET("/health", handleHealth)
-    }
+	apiGroup := router.Group("/app/cicd/api")
+	{
+		gh.Routes(apiGroup, ghHandler)
+		pipelineruns.Routes(apiGroup, pipelineRunsHandler)
+		apiGroup.GET("/health", handleHealth)
+	}
 
-    return router
+	return router
 }
 
 func handleHealth(ctx *gin.Context) {
-    response := dto.NewResponseDto(http.StatusOK, "I'm Alive!", "", "")
-    ctx.JSON(response.Status, response)
+	response := dto.NewResponseDto(http.StatusOK, "I'm Alive!", "", "")
+	ctx.JSON(response.Status, response)
 }
